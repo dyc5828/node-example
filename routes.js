fd7a754f1d3990bec54e31ad98c8cf559b3d658d
Daniel Chen <dyc5828@gmail.com>
2016-11-10 12:45:48 -0800

    starter

--- routes.js
+++ routes.js
@@ -1,11 +1,3 @@
 /**
  * Routes
  */
-const router = require('express').Router()
-const DvdController = require('./controllers/DvdController')
-const dvdController = new DvdController()
-
-router.get('/', dvdController.index)
-router.get('/detail/:id?', dvdController.detail)
-
-module.exports = router
